e2ff9301183e1f9378745795d61557e9033ea0f0
michaeld <michaeld@liveperson.com>
2017-11-05 13:51:47 +0200

    fixed some issues and added some tests

--- test/transformer_test.js
+++ test/transformer_test.js
@@ -2,9 +2,9 @@
 const expect = require('chai').expect;
 const mockery = require('mockery');
 const sinon = require('sinon');
+const facebookTransforme = require('./../lib/facebook');
 
 describe('Transformers Library Tests', () => {
-
 
     before(() => {
         mockery.enable({
@@ -19,7 +19,19 @@ describe('Transformers Library Tests', () => {
         mockery.disable();
     });
 
-    it('placeholder', done => {
+    it('should transform to facebook from liveperson', done => {
+        const lpCard = require('./lp-card.json');
+        const fbCard = require('./fb-card.json');
+        const fbCardNow = facebookTransforme.transformTo(lpCard);
+        expect(fbCardNow).to.deep.equal(fbCard);
+        done();
+    });
+
+    it('should transform to liveperson from facebook', done => {
+        const lpCard = require('./lp-card.json');
+        const fbCard = require('./fb-card.json');
+        const lpCardNow = facebookTransforme.transformFrom(fbCard);
+        expect(lpCardNow).to.deep.equal(lpCard);
         done();
     });
 
