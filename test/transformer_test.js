78c018fc6384ce35c3d957bf671784f0a57e26bb
natiz <natiiziv@gmail.com>
2017-11-06 19:10:13 +0200

    promisify facebook transformer

--- test/transformer_test.js
+++ test/transformer_test.js
@@ -2,7 +2,7 @@
 const expect = require('chai').expect;
 const mockery = require('mockery');
 const sinon = require('sinon');
-const facebookTransforme = require('./../lib/facebook');
+const facebookTransformer = require('./../lib/facebook');
 
 describe('Transformers Library Tests', () => {
 
@@ -22,17 +22,19 @@ describe('Transformers Library Tests', () => {
     it('should transform to facebook from liveperson', done => {
         const lpCard = require('./lp-card.json');
         const fbCard = require('./fb-card.json');
-        const fbCardNow = facebookTransforme.transformTo(lpCard);
-        expect(fbCardNow).to.deep.equal(fbCard);
-        done();
+        facebookTransformer.transformTo(lpCard).then(fbCardNow => {
+            expect(fbCardNow).to.deep.equal(fbCard);
+            done();
+        });
     });
 
     it('should transform to liveperson from facebook', done => {
         const lpCard = require('./lp-card.json');
         const fbCard = require('./fb-card.json');
-        const lpCardNow = facebookTransforme.transformFrom(fbCard);
-        expect(lpCardNow).to.deep.equal(lpCard);
-        done();
+        facebookTransformer.transformFrom(fbCard).then(lpCardNow => {
+            expect(lpCardNow).to.deep.equal(lpCard);
+            done();
+    });
     });
 
 });
